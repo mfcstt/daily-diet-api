d192fa5da3694dc30887ae878ea845a0508f1bbd
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2026-01-13 22:02:29 -0300

    chore: production config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,5 @@
 import { app } from "./app.js";
 import { env } from "./env.js";
-import cookie from "@fastify/cookie"
-
-app.register(cookie);
 
 app.listen({
   port: env.PORT, host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost'
